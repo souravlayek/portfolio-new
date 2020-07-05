a6a05b0d22f80854b603be5e617ef15cb2dac414
Sourav Layek <souravlayek11@gmail.com>
2020-07-06 00:06:59 +0530

    collect static setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
